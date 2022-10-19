335195cbab7dab3dc80f1c72ea5d1de7c63e2512
Max Kottong <max.kottong@gmail.com>
2022-10-18 22:43:54 -0400

    Node engine

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -61,8 +61,9 @@ module.exports = () => {
                     use: {
                         loader: 'babel-loader',
                         options: {
-                            presets: ['@babel/preset-env', {targets: "defaults"}]
-                            
+                            presets: [
+                                ['@babel/preset-env', { targets: "defaults" }]
+                            ]
                         }
                     }
                 }
